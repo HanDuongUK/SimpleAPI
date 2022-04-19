bf8f24ff1c1877b8ee478131a11ffa2f06ff15db
Han Duong <han.duong@aon.com>
2022-04-19 16:50:47 +0100

    Initial Commit

---
+++ test/SimpleAPI.Test/UnitTest1.cs
@@ -0,0 +1,21 @@
+using Xunit;
+using SimpleAPI.Controllers;
+
+namespace SimpleAPI.Test;
+
+public class UnitTest1
+{
+
+    [Fact]
+    public void GetReturnMyName()
+    {
+
+
+    }
+
+    [Fact]
+    public void Test1()
+    {
+
+    }
+}
